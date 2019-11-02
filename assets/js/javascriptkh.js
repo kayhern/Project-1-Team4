6c5288e4d93d31e1b7d7bbc7228c7ffee49dd434
Kristen Hernandez <kristen.e.hernandez@gmail.com>
2019-11-02 10:32:55 -0400

    button addition to html

--- assets/js/javascriptkh.js
+++ assets/js/javascriptkh.js
@@ -1,4 +1,8 @@
+console.log("JavaScript loaded");
+
 // everything the zip code functionality is doing should go here
 let zipcode = $("#inputZip");
-
-console.log(zipcode)
+console.log('zip code:', zipcode)
+// Add button to submit input
+// Setup onClick function for that button
+// Pull the zipcode value from there and do something
