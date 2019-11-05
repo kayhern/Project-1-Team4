d5909a4744eaa2ae96261f2f58799e8a67a21812
Kristen Hernandez <kristen.e.hernandez@gmail.com>
2019-11-05 18:02:30 -0500

    added mario javascript and check box html

--- assets/js/javascriptkh.js
+++ assets/js/javascriptkh.js
@@ -25,6 +25,36 @@ $(document).ready(function () {
         };
     });
 
+    // define an empty array to which to append foods
+    let foodList = []
+    // add event listener for clicks on checkboxes 
+    $(".form-check-input").on("click", function () {
+        // evaluate the "cheked" attribute of the box clicked
+        // if the box is checked and the value of the box is not contained in the food array 
+        // append the value of the box tot he food awway 
+        if (this.checked && !foodList.includes($(this).val())) {
+            // obtained the value of the box clicked AKA: the food type
+            let newFood = $(this).val();
+            // push food into the array 
+            foodList.push(newFood)
+            // console log the array
+            console.log(foodList)
+            // check if the the box is unchecked
+        } else if (!this.checked) {
+            // loop over the array until the value of the 
+            // unchecked box if found
+            for (var i = 0; i < foodList.length; i++) {
+                if (foodList[i] === $(this).val()) {
+                    // remove this food from the array 
+                    foodList.splice(i, 1);
+                }
+            }
+            // console log the new array 
+            console.log(foodList)
+        }
+    });
+
+
     $("#button").on("click", function () {
         let zipCode = $("#inputZip").val();
         console.log(zipCode);
