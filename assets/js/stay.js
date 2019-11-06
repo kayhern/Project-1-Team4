75312d048fe94f756e175ff8cedbe9ae3dd53b85
TheRealCalvinKO <calvinknockout@gmail.com>
2019-11-05 19:18:12 -0500

    edited

--- assets/js/stay.js
+++ assets/js/stay.js
@@ -1,10 +1,4 @@
-var button = document.createElement("stay");
-button.innerHTML = "stay";
-
-var body = document.getElementsByTagName("stay")[0];
-body.appendChild(button);
-
-button.addEventListener ("click", function() {
-  alert("stay");
+$("#stay").on("click", function () {
+  let stay = $("#stay").val();
+  console.log(stay);
 });
-
